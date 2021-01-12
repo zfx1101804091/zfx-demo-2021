b5207b8953be8755fea3adf67d1fcf5fc9f006bd
zhengfengxiang <1875586562@qq.com>
2021-01-12 18:40:31 +0800

    io操作 递归读取文件夹下的某一个文件

--- demo-fileIo/src/main/java/com/zfx/io/Test.java
+++ demo-fileIo/src/main/java/com/zfx/io/Test.java
@@ -9,14 +9,23 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) throws IOException {
 //        String path ="D:\\RuoYiCloud\\ruoyi-api";
-        String path ="D:\\zfx\\调研文档";
-        traverseFolder1(path,"linux相关命令.md");
+
+        String path = "D:\\zfx\\调研文档";
+//        traverseFolder1(path,"linux相关命令.md");
+
+        File root=new File(path);
+        List<String> fileNameList=new ArrayList<String>();
+        showFileList(root,fileNameList);
+        for(String fileName:fileNameList){
+            System.out.println(fileName);
+        }
+
     }
 
-    public static List<String > traverseFolder1(String path,String fileName) throws IOException {
-        List<String> fileList=new ArrayList<String>();
+    public static List<String> traverseFolder1(String path, String fileName) throws IOException {
+        List<String> fileList = new ArrayList<String>();
         int fileNum = 0, folderNum = 0;
-        String filePath ="";
+        String filePath = "";
         File file = new File(path);
         if (file.exists()) {
             LinkedList<File> list = new LinkedList<File>();
@@ -27,8 +36,8 @@ public class Test {
                     folderNum++;
                 } else {
                     fileList.add(file2.getAbsolutePath());
-                    if(file2.getName().equals(fileName)){
-                        filePath=file2.getCanonicalPath();
+                    if (file2.getName().equals(fileName)) {
+                        filePath = file2.getCanonicalPath();
                     }
 
                     fileNum++;
@@ -45,8 +54,8 @@ public class Test {
                     } else {
                         fileList.add(file2.getAbsolutePath());
                         fileList.add(file2.getAbsolutePath());
-                        if(file2.getName().equals(fileName)){
-                            filePath=file2.getCanonicalPath();
+                        if (file2.getName().equals(fileName)) {
+                            filePath = file2.getCanonicalPath();
                         }
                         fileNum++;
                     }
@@ -55,7 +64,27 @@ public class Test {
         } else {
             System.out.println("文件不存在!");
         }
-        System.out.println("文件夹共有:" + folderNum + ",文件共有:" + fileNum+",文件绝对路径："+filePath);
+        System.out.println("文件夹共有:" + folderNum + ",文件共有:" + fileNum + ",文件绝对路径：" + filePath);
         return fileList;
     }
+
+
+    public static void showFileList(File file,List<String> fileNameList){
+        if(!file.exists()){
+            System.out.println("这是一个错误的路径");
+        }
+        if(file.isDirectory()){
+            File []files=file.listFiles();
+            for (File file2 : files) {
+                showFileList(file2,fileNameList);
+            }
+        }else{
+            //System.out.println(file.getAbsolutePath());
+            //System.out.println(file.getPath()+File.separator+file.getName());
+            if(file.isFile()){
+                fileNameList.add(file.getAbsolutePath());
+            }
+        }
+    }
+
 }
